9b459b332021533de7402a6283f4e0e06b1cf4b9
SuperITMan <admin@superitman.com>
2020-11-13 12:18:31 +0000

    ci - update application Fennec_F-Droid with org.mozilla.fennec_fdroid

--- Fennec_F-Droid/Android.mk
+++ Fennec_F-Droid/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Fennec_F-Droid
-LOCAL_SRC_FILES := org.mozilla.fennec_fdroid_821120.apk
+LOCAL_SRC_FILES := org.mozilla.fennec_fdroid_821320.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
