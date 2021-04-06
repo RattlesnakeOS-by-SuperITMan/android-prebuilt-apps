ac483bc86fd8cf1fc1a2654c85dfccb8500d3de0
SuperITMan <admin@superitman.com>
2021-04-06 12:31:07 +0000

    ci - update application Simple-Calculator with com.simplemobiletools.calculator

--- Simple-Calculator/Android.mk
+++ Simple-Calculator/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Calculator
-LOCAL_SRC_FILES := com.simplemobiletools.calculator_39.apk
+LOCAL_SRC_FILES := com.simplemobiletools.calculator_40.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
